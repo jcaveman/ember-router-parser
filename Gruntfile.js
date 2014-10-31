a482e52a7c14a08fd35d4636401f2454984a2539
Adrian Ancona Novelo <anovelo@linkedin.com>
2014-10-31 16:14:24 -0700

    Added gruntfile with eslint

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = function(grunt) {
+  /**
+   * This function grabs all files under the given path and uses them as
+   * configurations for grunt tasks
+   * @params {string} path - Path were configuration files live
+   */
+  function loadConfig(path) {
+    var object = {};
+    var key;
+
+    var files = grunt.file.expand({cwd: path}, '*');
+    files.forEach(function(option) {
+      key = option.replace(/\.js$/, '');
+      object[key] = require(path + option);
+    });
+
+    return object;
+  }
+
+  grunt.initConfig(loadConfig('./config/grunt/'));
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('health', ['eslint']);
+};
---
+++ config/grunt/eslint.js
@@ -0,0 +1,7 @@
+module.exports = {
+  target: [
+    'Gruntfile.js',
+    'config/grunt',
+    'bin'
+  ]
+};
